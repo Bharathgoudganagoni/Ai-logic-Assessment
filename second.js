823778f3a775515b099dd3fb06f5d22986cfb7cd
Bharathgoudganagoni <bharathgoudganagoni123@gmail.com>
2026-09-07 12:06:43 +0530

    Initial commit

---
+++ second.js
@@ -0,0 +1,21 @@
+function majorityElement(nums) {
+    let candidate = null;
+    let count = 0;
+
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+
+        if (num === candidate) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+
+    return candidate;
+}
+
+const nums = [2, 2, 1, 1, 1, 2, 2];
+console.log(majorityElement(nums));
