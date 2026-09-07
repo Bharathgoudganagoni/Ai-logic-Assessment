21dff2dc990d60023892a4d0781c767b7dfadd97
Bharathgoudganagoni <bharathgoudganagoni123@gmail.com>
2026-09-07 12:19:04 +0530

    3rd,4th question updated

---
+++ fourth.js
@@ -0,0 +1,26 @@
+function maxArea(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let max = 0;
+
+    while (left < right) {
+        const width = right - left;
+        const h = Math.min(height[left], height[right]);
+        const area = width * h;
+
+        if (area > max) {
+            max = area;
+        }
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return max;
+}
+
+const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+console.log(maxArea(height));
---
+++ third.js
@@ -0,0 +1,25 @@
+function maximumSwap(num) {
+    const digits = String(num).split('');
+    const last = new Array(10).fill(-1);
+
+    for (let i = 0; i < digits.length; i++) {
+        last[digits[i]] = i;
+    }
+
+    for (let i = 0; i < digits.length; i++) {
+        for (let d = 9; d > digits[i]; d--) {
+            if (last[d] > i) {
+                const temp = digits[i];
+                digits[i] = digits[last[d]];
+                digits[last[d]] = temp;
+
+                return Number(digits.join(''));
+            }
+        }
+    }
+
+    return num;
+}
+
+const num = 2736;
+console.log(maximumSwap(num));
